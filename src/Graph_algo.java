ed1b2ca71ecc83db24bd39cb35d84495bbc6ded8
Or Evron <orevron@gmail.com>
2016-11-15 10:26:17 +0200

    cosmetics fixing and TODO comment!!

--- src/Graph_algo.java
+++ src/Graph_algo.java
@@ -1,36 +1,36 @@
 import java.util.List;
 
+
+//TODO: ALL METHODS SHOULD BE STATIC,   ARGUMENTS SHOULD BE (Graph graph, int VertexA, int VertexB)
+
+
 public class Graph_algo {
     private Dijkstra.DijkstraSP dsp;
 
     public Graph_algo(String path) {
-        Graph graph = new Graph(path);
-        dsp = new Dijkstra.DijkstraSP(graph.getVertecies());
-
+        dsp = new Dijkstra.DijkstraSP((new Graph(path)).getVertecies());
     }
 
-    public double calcDistBetweenTwoVertex(int vertexA, int vertexB) {
-
+    public double shortestCost(int vertexA, int vertexB) {
         dsp.computePaths(dsp.getVertex(vertexA));
         Dijkstra.Vertex vertex = dsp.getVertex(vertexB);
-
         return vertex.minDistance;
     }
 
-    public String calcBestPath(int vertexA, int vertexB) {
-
+    public String shortestPath(int vertexA, int vertexB) {
         dsp.computePaths(dsp.getVertex(vertexA));
         Dijkstra.Vertex vertex = dsp.getVertex(vertexB);
         List<Dijkstra.Vertex> path = dsp.getShortestPathTo(vertex);
-
         return path.toString();
-
     }
 
+//    public String shortestPathExcludeBlacklist(int vertexA, int vertexB, List blackList) {
+//
+//    }
 
     public static void main(String[] args) {
         Graph_algo graph_algo = new Graph_algo("exampleFiles\\G0.txt");
-//        System.out.println("calcDistBetweenTwoVertex: " + graph_algo.calcDistBetweenTwoVertex(0, 1));
-        System.out.println("calcBestPath: " + graph_algo.calcBestPath(0, 1));
+        System.out.println("shortestCost: " + graph_algo.shortestCost(0, 1));
+        System.out.println("shortestPath: " + graph_algo.shortestPath(0, 1));
     }
 }
